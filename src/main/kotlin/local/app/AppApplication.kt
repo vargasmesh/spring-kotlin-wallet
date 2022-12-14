57d686a354fbc670cb76371647ec9b3eaf752310
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2022-12-14 08:14:58 -0300

    feat: Hello World with GraphQL

--- src/main/kotlin/local/app/AppApplication.kt
+++ src/main/kotlin/local/app/AppApplication.kt
@@ -2,10 +2,22 @@ package local.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.stereotype.Controller
 
 @SpringBootApplication
 class AppApplication
 
 fun main(args: Array<String>) {
 	runApplication<AppApplication>(*args)
+}
+
+
+@Controller
+class HelloController {
+
+	@QueryMapping
+	fun hello(): String {
+		return "World"
+	}
 }
