5da949dd75287d421418c657b7065ccc5b5964a4
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2022-12-18 16:22:21 -0300

    feat: Handle insufficient funds

--- src/main/kotlin/local/app/adapter/graphql/AccountResolver.kt
+++ src/main/kotlin/local/app/adapter/graphql/AccountResolver.kt
@@ -1,6 +1,10 @@
 package local.app.adapter.graphql
 
+import graphql.GraphQLError
+import graphql.GraphqlErrorBuilder
+import graphql.schema.DataFetchingEnvironment
 import local.app.application.command.AccountCommands
+import local.app.application.command.DebitError
 import local.app.application.query.AccountQueries
 import local.app.domain.model.*
 import org.javamoney.moneta.FastMoney
@@ -8,8 +12,28 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter
+import org.springframework.graphql.execution.ErrorType
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Controller
 
+@Component
+class AccountExceptionResolver: DataFetcherExceptionResolverAdapter() {
+    override fun resolveToSingleError(ex: Throwable, env: DataFetchingEnvironment): GraphQLError? {
+        return when(ex) {
+            is InsufficientFunds -> GraphqlErrorBuilder.newError()
+                .errorType(ErrorType.BAD_REQUEST)
+                .message("insufficient funds")
+                .build()
+            else -> GraphqlErrorBuilder.newError()
+                .errorType(ErrorType.INTERNAL_ERROR)
+                .message("something went wrong")
+                .build()
+        }
+    }
+}
+
+class InsufficientFunds(message: String = "insufficient funds"): RuntimeException()
 
 @Controller
 class AccountResolver(
@@ -26,8 +50,12 @@ class AccountResolver(
     @MutationMapping
     fun debitAccount(@Argument accountID: String, @Argument amount: String): Account {
         val account = accountQueries.getAccount(accountID)
-        accountCommands.debitAccount(DebitAccountEvent(account, FastMoney.of(amount.toDouble(), currency) ))
-        return accountQueries.getAccount(accountID)
+
+        val error = accountCommands.debitAccount(DebitAccountEvent(account, FastMoney.of(amount.toDouble(), currency) ))
+        return when(error) {
+            is DebitError.InsufficientFunds -> throw InsufficientFunds()
+            else -> accountQueries.getAccount(accountID)
+        }
     }
 
     @MutationMapping
